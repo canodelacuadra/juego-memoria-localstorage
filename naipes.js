41468daef2f9a0f63997fdf71081eb48b5c730c1
canodelacuadra <canodelacuadra@gmail.com>
2024-04-07 13:28:08 +0200

    paso 2 muestro cartas barajadas

--- naipes.js
+++ naipes.js
@@ -1,36 +1,36 @@
- const naipes = [
+  export const reverso ='./imagenes/reverso.jpg'
+const naipes = [
     
+  
     {
-        reverso: 'reverso',
-        url: './imagenes/reverso.jpg'
-    },
-    {
-        naipe01: 'marilin01',
+        title: 'Marilin Monroe',
         url: './imagenes/marilin01.jpg'
     },
     {
-        naipe01: 'marilin02',
+        title: 'Marilin Monroe',
         url: './imagenes/marilin02.jpg'
     },
     {
-        naipe01: 'marilin03',
-        url: './imagenes/marilin03.jpg'
+        title: 'Brigitte Bardot',
+        url: './imagenes/brigitte-bardot01.jpg'
     },
       {
-        naipe01: 'marilin04',
-        url: './imagenes/marilin04.jpg'
+        title: 'Brigitte Bardot',
+        url: './imagenes/brigitte-bardot02.jpg'
     },
         {
-        naipe01: 'marilin05',
-        url: './imagenes/marilin05.jpg'
+        title: 'Scarlett Johansson',
+        url: './imagenes/scarlett-johansson01.jpg'
     },
     
           {
-        naipe01: 'marilin06',
-        url: './imagenes/marilin06.jpg'
+        title: 'Scarlett Johansson',
+        url: './imagenes/scarlett-johansson02.jpg'
     }
     
 
 
 ]
 export default naipes
+ 
+    
--- script.js
+++ script.js
@@ -1,12 +1,43 @@
 import naipes from './naipes.js'
-console.log(naipes)
+import { reverso } from './naipes.js';
+console.log(reverso)
+
 // cargamos los elementos del DOM del index 
 const tablero = document.querySelector(".tablero");
 const puntuacionElement = document.getElementById("puntuacion");
-// mostrar naipes
-// barajar naipes
+function jugar() {
+    mostrarNaipes(naipes, reverso);
+    
+}
+//jugar();
+document.getElementById('btn-play').addEventListener('click', jugar)
+// mostrar naipes - mostrará el doble de naipes de los naipes dados 
+function mostrarNaipes(naipes, reverso) {
+     // Barajar las naipes
+    naipes = naipes.sort(() => Math.random() - 0.5);
+    
+    // creamos en html y ocultamos los naipes colocando encima el reverso
+    naipes.map(naipe => {
+        //creamos un template accediendo al nombre y al url de cada imagen
+        let templateImg = `
+        <figure class='naipe'>
+        <img class='frente' alt ='${naipe.nombre}'src='${naipe.url}'/>
+        <img  class='reverso'src='${reverso}'/>
+        </figure>
+        `
+        // se lo agragamos al tablero
+        tablero.innerHTML += templateImg
+    
+    }
+     )
+    
+}
+// los naipes se muestran durante un tiempo x
+function mostrarNaipesInicio(naipes, time) {
+  
+}
 // ocultar naipes
-// encontrar parejas
-// si se encuentra pareja y no se oculta
+// encontrar parejas y se almacena en localstorage
+// si se encuentra pareja y no se oculta 
 // si no se encuentra no se oculta
 // todas encontradas se gana
