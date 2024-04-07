34f8fe028bb98b5ab22622960baf8fccf42f9071
canodelacuadra <canodelacuadra@gmail.com>
2024-04-07 10:35:30 +0200

    paso 1 codigo de inicio

---
+++ naipes.js
@@ -0,0 +1,36 @@
+ const naipes = [
+    
+    {
+        reverso: 'reverso',
+        url: './imagenes/reverso.jpg'
+    },
+    {
+        naipe01: 'marilin01',
+        url: './imagenes/marilin01.jpg'
+    },
+    {
+        naipe01: 'marilin02',
+        url: './imagenes/marilin02.jpg'
+    },
+    {
+        naipe01: 'marilin03',
+        url: './imagenes/marilin03.jpg'
+    },
+      {
+        naipe01: 'marilin04',
+        url: './imagenes/marilin04.jpg'
+    },
+        {
+        naipe01: 'marilin05',
+        url: './imagenes/marilin05.jpg'
+    },
+    
+          {
+        naipe01: 'marilin06',
+        url: './imagenes/marilin06.jpg'
+    }
+    
+
+
+]
+export default naipes
---
+++ script.js
@@ -0,0 +1,12 @@
+import naipes from './naipes.js'
+console.log(naipes)
+// cargamos los elementos del DOM del index 
+const tablero = document.querySelector(".tablero");
+const puntuacionElement = document.getElementById("puntuacion");
+// mostrar naipes
+// barajar naipes
+// ocultar naipes
+// encontrar parejas
+// si se encuentra pareja y no se oculta
+// si no se encuentra no se oculta
+// todas encontradas se gana
